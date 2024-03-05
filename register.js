d1eab69ae0b02a1f537e7197e37ab031cbd529cd
Symon Kyle Salvador <salvadorkyle.wup@gmail.com>
2024-03-05 14:49:38 +0800

    second commit

--- register.js
+++ register.js
@@ -9,20 +9,36 @@
             // undefined = null
 
 
-            console.log("Hello World", ["apple", "banana", "grape"]);
+            //console.log("Hello World", ["apple", "banana", "grape"]);
             //console.error("Hello World");
             //console.warn("Hellow World");
-            console.table(["apple", "banana", "grape"]);
+            //console.table(["apple", "banana", "grape"]);
             //let name = true;
             //console.log(typeof(name));
-            let txtUsename = document.querySelector("#txtUsername");
-            let btnRegister = document.querySelector("#btnRegister");
+            let txtUsename = document.querySelector("#txtUsername")
+            let txtPassword = document.querySelector("#txtPassword")
+            let txtConfirmPassword = document.querySelector("#txtConfirmPassword")
+            let btnRegister = document.querySelector("#btnRegister")
         
             btnRegister.onclick = function () {
-                register(txtUsename.value)
+                register(txtUsename.value, txtPassword.value, txtConfirmPassword.value)
             }
             //variables
 
-            function register (username) {
-                console.log(username)
+            function register (username, password, confirmPassword) {
+                //....... If else........ if else
+                // Conditional Operators ( Greater Than >, Less Than<, Equal ==, Not !, Or || )
+                // Greater Than or Equal >=, Less Than or Equal <=
+                // Not Equal !=
+                console.log(username, password, confirmPassword)
+
+                if (password == confirmPassword && password != "" && confirmPassword != "" ) {
+                    return console.log ("Password Match!")
+                } else { if (password == "" || confirmPassword == ""){
+                    return console.log ("Password must not be empty.")
+                }
+                else {
+                    return console.log ("Password does not Match.")
+                }    
             }
+        }
